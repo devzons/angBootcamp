18131c0790f75e055b2705f73feee6b0811d35a7
Don Yu <devzons@gmail.com>
2018-11-22 15:47:40 -0800

    BMI...

--- bmiCalc/server.js
+++ bmiCalc/server.js
@@ -10,10 +10,10 @@ app.get("/", function(req, res){
 
 app.post("/bmicalculator", function(req, res){
   
-  var weight = Number(req.body.weight);
-  var height = Number(req.body.height);
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
 
-  var result = Math.round( weight / (height * height));
+  var result =  weight / (height * height);
 
     res.send("Your BMI is " + result);
 });
